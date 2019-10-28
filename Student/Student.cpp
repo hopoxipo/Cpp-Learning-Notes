ef2896dda59eb6935af9e255a06e97724cececa3
hopoxipo <54829950+hopoxipo@users.noreply.github.com>
2019-10-28 12:02:17 +0800

    Create Student.cpp

---
+++ Student/Student.cpp
@@ -0,0 +1,16 @@
+#include "Student.h"
+#include <vector>
+
+int main()
+{
+    while (true)
+    {
+        display();
+        switch()
+    }
+    Student stu1(1, "Alice", 89);
+    Student stu2(2, "Bob", 78);
+    Student stu3(3, "Cathy", 90);
+    std::vector<Student> stuvec = { stu1, stu2, stu3 };
+    sys(stuvec);
+}
